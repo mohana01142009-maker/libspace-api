4e328845a67d8e1529113a60c6400e89469502d2
mohana01142009-maker <mohana01142009@gmail.com>
2026-03-08 16:26:27 +0530

    Initial libspace API project upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./Config/db.js";
+import userRouter from "./routes/user.route.js";
+import bookRouter from "./routes/book.route.js";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+// Root route
+app.get("/",(req, res) => {
+  res.send("libSpace API Running");
+});
+
+//Routes
+app.use("/api/authuser", userRouter);
+app.use("/api/book",bookRouter);
+
+//start server
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Our server is working at PORT :${PORT}`);
+});
+
+
